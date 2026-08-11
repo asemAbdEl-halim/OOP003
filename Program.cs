66f6c33163e17a8b43d1f9528d61c0b5da7b559f
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-12 01:10:39 +0300

    Quetion01_Part01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            #region Quetion01
+
+            /*
+             * 1 - a)  What is the difference between Method Overloading and Method Overriding?
+             * ==> Answer <==
+             * ==> Overloading: Same method name but different parameters in the same class.
+             * ==> Overriding: A child class changes the implementation of a method from the parent class.
+             * 
+             * 1 - b) What is the difference between Static Binding and Dynamic Binding?
+             * => Answer <==
+             * Static Binding: The method to call is decided at compile time.
+             * Dynamic Binding: The method to call is decided at runtime, usually with virtual and override.
+             * **/
+
+            #endregion
+
+            #endregion
         }
     }
 }
